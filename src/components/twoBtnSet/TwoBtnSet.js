06ffa089c9457e860a4bec35c3b90c64f4d70820
Ian Sutherland <iansutherland.az@gmail.com>
2019-08-29 22:13:37 -0700

    started a twobtnset component for repetitive side by side add/update/cancel btns

---
+++ src/components/twoBtnSet/TwoBtnSet.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Consumer } from '../../Context';
+
+export default class TwoBtnSet extends Component {
+    render() {
+        return(
+            {value => {
+
+            }}
+        )
+    }
+}
--- src/components/withdrawn/Withdrawn.js
+++ src/components/withdrawn/Withdrawn.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { Consumer } from '../../Context';
 
 export default class Withdrawn extends Component {
+
+    onUpdate = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
     render() {
         return (
             <Consumer>
@@ -9,16 +14,44 @@ export default class Withdrawn extends Component {
                     const { dispatch } = value;
 
                     return <div className="row">
-                        <div className="col-md-4">
+                        <div className="col-md-6">
                             <h6>Add a new Record</h6>
                             <hr/>
                             <div className="row">
-                                <label>
+                                <label className="col-md-6">
                                     <input type="radio"></input>
                                     Single Well Reading
                                 </label>
+                                <label className="col-md-6">
+                                    <input type="radio"></input>
+                                    Well Groups
+                                </label>
                             </div>
-                            <input type="text" placeholder="Well # 55-XXXXXX"></input>
+                            <div className="row">
+                                <input type="text" placeholder="Well # 55-XXXXXX" onChange={this.onUpdate.bind(this)}></input>
+                            </div>
+                            <div className="row">
+                                <input type="text" placeholder="Water Quantity" onChange={this.onUpdate.bind(this)}></input>
+                            </div>
+                            <div className="row">
+                            <select className="text-center col-md-7" onChange={this.onUpdate.bind(this)}>
+                                <option value="Acre Feet">Acre Feet</option>
+                                <option value="Gallons">Gallons</option>
+                            </select>
+                            </div>
+                            <div className="row">
+                            <label className="col-md-6">
+                                    <input type="radio"></input>
+                                    Metered
+                                </label>
+                                <label className="col-md-6">
+                                    <input type="radio"></input>
+                                    Estimated
+                                </label>
+                            </div>
+                            <div className="row col-md-12">
+                                <textarea placeholder="Comments"></textarea>
+                            </div>
                         </div>
                         <div class="col-md-4"></div>
                     </div>
