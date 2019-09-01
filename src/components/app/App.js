f935f102b7d002521af4391f24bbea68677603de
Ian Sutherland <iansutherland.az@gmail.com>
2019-08-31 17:22:01 -0700

    changed main jumbotron width to 75%

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,7 +6,7 @@ export default (props) => {
   return (
     <div className='h-100 w-100 container-fluid ' id='app-div'>
       <Navbar />
-      <div className="jumbotron w-50 m-auto bg-light pt-1">
+      <div className="jumbotron w-75 m-auto bg-light pt-1">
         <h4>WELCOME TO THE ARIZONA DEPARTMENT OF WATER RESOURCES</h4>
         <h6>2018 ELECTRONIC ANNUAL REPORT PROCESS</h6>
         <hr></hr>
